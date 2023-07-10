8ff14136acfedd9ea849786571ba0698354a7851
Kian <116115355+Kian-2108@users.noreply.github.com>
2023-07-10 12:33:55 +0000

    updated paths

--- test.py
+++ test.py
@@ -17,14 +17,16 @@ os.environ["GOOGLE_API_KEY"] = "AIzaSyD6lORTrf5wLPP6wR6keH6yhP2Kwd-A1r4"
 llm = GooglePalm(model_name = "models/text-bison-001",temperature = 0.1)
 embeddings = GooglePalmEmbeddings(model_name="models/embedding-gecko-001")
 
-for file in tqdm(file_names):
-    loader = PyPDFLoader(f"./data/{file}")
-    documents = loader.load()
-    text_splitter = RecursiveCharacterTextSplitter()
-    texts = text_splitter.split_documents(documents)
-    docsearch = FAISS.from_documents(texts, embeddings)
-    docsearch.save_local(folder_path='FAISS_VS', index_name=f"{file.split('.')[0]}_index")
-    print(file.split(".")[0])
+# for file in tqdm(file_names):
+#     loader = PyPDFLoader(f"./data/{file}")
+#     documents = loader.load()
+#     text_splitter = RecursiveCharacterTextSplitter()
+#     texts = text_splitter.split_documents(documents)
+#     docsearch = FAISS.from_documents(texts, embeddings)
+#     docsearch.save_local(folder_path='FAISS_VS', index_name=f"{file.split('.')[0]}_index")
+#     print(file.split(".")[0])
 
-# docsearch = FAISS.load_local(folder_path='FAISS_VS', embeddings=embeddings, index_name='BMO_AR2022')
-# retriever = docsearch.as_retriever()
+docsearch = FAISS.load_local(folder_path='./FAISS_VS', embeddings=embeddings, index_name=f"{file_names[0].split('.')[0]}_index")
+retriever = docsearch.as_retriever()
+
+print(retriever.get_relevant_documents("Which reports bank BMO has to send to OSFI for BCAR Credit Risk?"))
