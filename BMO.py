1dac5698aa0fe5a76bf16d67838a0cc45e3a09be
Kian <116115355+Kian-2108@users.noreply.github.com>
2023-07-10 14:24:13 +0000

    updated ui

--- BMO.py
+++ BMO.py
@@ -100,15 +100,16 @@ if 'generated' not in st.session_state:
 ## past stores User's questions
 if 'past' not in st.session_state:
     st.session_state['past'] = []
-user_input = st.empty()
+messages = st.container()
+user_input = st.text_input("You: ","", key="input")
 if user_input:
     output = agent.run(user_input)
     st.session_state.past.append(user_input)
     st.session_state.generated.append(output)
 if 'generated' in st.session_state:
-    for i in range(len(st.session_state['generated'])):
-        message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
-        message(st.session_state["generated"][i], key=str(i))
-user_input.text_input("You: ", "", key="input")
+    with message:
+        for i in range(len(st.session_state['generated'])):
+            message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
+            message(st.session_state["generated"][i], key=str(i))
 
 # agent.run("Which reports bank BMO has to send to OSFI for BCAR Credit Risk?")
